d2e07c2890691dc14ef95f3650a4604baf8cc5b1
Yoksha81 <jbokshan003@gmail.com>
2026-05-16 13:47:50 +0200

    Dodato jos par metrika

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import cv2
 import pandas as pd
+import numpy as np
+from scipy import ndimage
+from timeit import default_timer as timer
+
 
 ROOT = Path(__file__).resolve().parent.parent
 VIDEO_PATH = ROOT / "data" / "raw_videos"
@@ -40,23 +44,74 @@ def analiza_jednog_videa(video_path):
     sirina = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     visina = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+    step = round(fps)
+    L = np.array([[0,1,0],[1,-4,1],[0,1,0]])
+    k=0
+    prag = 0.3
+    brightness_values = []
+    laplasian_variance = []
+    motion_between_frames = []
+    frame_motion = []
+    active_frame = 0
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        sivi_frejm = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        if k % step == 0:
+            laplasian_variance.append(np.var(ndimage.convolve(sivi_frejm.astype('float'),L,mode='mirror')))
+
+        brightness_values.append(sivi_frejm.mean())
+
+        if k == 0:
+            prethodni_frejm = sivi_frejm
+            k=1
+            continue
+
+        trenutni_frejm = sivi_frejm
+
+        pokret = np.mean(abs(trenutni_frejm.astype('float') - prethodni_frejm.astype('float')))
+        motion_between_frames.append(pokret)
+        frame_motion.append(pokret/255)
+
+        active_frame += frame_motion[-1] > prag
+
+        prethodni_frejm = sivi_frejm
+
+        k+=1
+
+    avg_brightness = sum(brightness_values) / len(brightness_values)
+
     row = {
         "filename": filename,
         "fps": fps,
         "ukupno_frejmova": ukupno_frejmova,
         "trajanje_videa": trajanje_videa,
         "sirina": sirina,
-        "visina": visina
+        "visina": visina,
+        "avg_brightness": avg_brightness,
+        "sharpness_score": np.mean(laplasian_variance),
+        "motion_score": np.mean(frame_motion),
+        "min_frame_motion": np.min(frame_motion),
+        "max_frame_motion": np.max(frame_motion),
+        "active_frame_ratio": active_frame/ukupno_frejmova
     }
     cap.release()
     return row
 
 def main():
+    start_time = timer()
     rezultati = analiza_svih_videa(VIDEO_PATH)
     df = pd.DataFrame(rezultati)
     OUT_PATH.parent.mkdir(parents=True, exist_ok=True)
     df.to_csv(OUT_PATH, index=False)
-    print("Gascina sve radi kako treba")
+    end_time = timer()
+    print(f"Gascina sve radi kako treba, vreme potrebno za izvrsavanje {end_time-start_time}")
+
 
 if __name__ == "__main__":
     main()
