eae7df718edb04c42069ea6bb590304666a9b9d7
Yoksha81 <jbokshan003@gmail.com>
2026-05-19 10:50:33 +0200

    Promenio active_f_r i dodao motion area score

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ from roipoly import RoiPoly
 
 ROOT = Path(__file__).resolve().parent.parent
 VIDEO_PATH = ROOT / "data" / "raw_videos"
-OUT_PATH = ROOT / "data" / "processed" / "analiza_videa.csv"
+OUT_PATH = ROOT / "data" / "processed" / "analiza_videa_step1.csv"
 VIDEO_EXTENSIONS = {".mp4", ".mkv", ".mov", ".avi"}
 
 def roi_maska(video_path):
@@ -37,16 +37,30 @@ def roi_maska(video_path):
     plt.axis('off')
 
     my_roi = RoiPoly(color='r')
+
+    print("ROI x:", my_roi.x)
+    print("ROI y:", my_roi.y)
+    x_min = min(my_roi.x).astype(int)
+    x_max = max(my_roi.x).astype(int)
+    y_min = min(my_roi.y).astype(int)
+    y_max = max(my_roi.y).astype(int)
+    print("x_min:", x_min.astype(int))
+    print("x_max:", x_max.astype(int))
+    print("y_min:", y_min.astype(int))
+    print("y_max:", y_max.astype(int))
+
     maska = my_roi.get_mask(gray)
+
+    povrsina = (x_max - x_min)*(y_max - y_min)
 
     plt.figure()
     plt.imshow(maska)
     plt.show()
 
-    return maska
+    return maska, x_min, x_max, y_min, y_max, povrsina
 
 
-def analiza_jednog_videa(video_path, maska):
+def analiza_jednog_videa(video_path, maska, x_min, x_max, y_min, y_max, povrsina):
 
     filename = video_path.name
     cap = cv2.VideoCapture(str(video_path))
@@ -67,15 +81,20 @@ def analiza_jednog_videa(video_path, maska):
     sirina = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     visina = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    step = round(fps)
-    L = np.array([[0,1,0],[1,-4,1],[0,1,0]])
+    brightness_step = round(fps)
+    motion_step = 4
+    #L = np.array([[0,1,0],[1,-4,1],[0,1,0]])
     k=0
-    prag = 0.3
+    prag = 1.18
     brightness_values = []
     laplasian_variance = []
-    motion_between_frames = []
+    #motion_between_frames = []
     frame_motion = []
     active_frame = 0
+    br_poredjenja = 0
+    motion_area_score = []
+    pixel_diff_threshold = 0.2
+    area_threshold = 0.04
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -83,27 +102,29 @@ def analiza_jednog_videa(video_path, maska):
         if not ret:
             break
 
-        sivi_frejm = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) * maska
+        sivi_frejm = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        bbox_frejm = sivi_frejm[y_min:y_max, x_min:x_max] * maska[y_min:y_max, x_min:x_max]
 
-        if k % step == 0:
-            laplasian_variance.append(np.var(ndimage.convolve(sivi_frejm.astype('float'),L,mode='mirror')))
+        brightness_values.append(bbox_frejm.mean())
 
-        brightness_values.append(sivi_frejm.mean())
+        if k % brightness_step == 0:
+            laplasian_variance.append(cv2.Laplacian(src=bbox_frejm, ddepth=-1, ksize=1).var())
 
         if k == 0:
-            prethodni_frejm = sivi_frejm
+            prethodni_frejm = bbox_frejm
             k=1
             continue
 
-        trenutni_frejm = sivi_frejm
+        if k % motion_step == 0:
+            abs_razlika = abs(bbox_frejm.astype('float') - prethodni_frejm.astype('float'))
+            motion_area_score.append(sum(abs_razlika > pixel_diff_threshold) / povrsina)
+            frame_motion.append(np.mean(abs_razlika))
+            active_frame += frame_motion[-1] > prag
+            br_poredjenja += 1
 
-        pokret = np.mean(abs(trenutni_frejm.astype('float') - prethodni_frejm.astype('float')))
-        motion_between_frames.append(pokret)
-        frame_motion.append(pokret/255)
+        #TODO event detection -- izracunam za poslednjih 15 frejmova statistike
 
-        active_frame += frame_motion[-1] > prag
-
-        prethodni_frejm = sivi_frejm
+        prethodni_frejm = bbox_frejm
 
         k+=1
 
@@ -121,19 +142,20 @@ def analiza_jednog_videa(video_path, maska):
         "motion_score": np.mean(frame_motion),
         "min_frame_motion": np.min(frame_motion),
         "max_frame_motion": np.max(frame_motion),
-        "active_frame_ratio": active_frame/ukupno_frejmova
+        "active_frame_ratio": active_frame/br_poredjenja,
+        "mean_motion_area_score": np.mean(motion_area_score)
     }
     cap.release()
     return row
 
 
-def analiza_svih_videa(video_folder, maska):
+def analiza_svih_videa(video_folder, maska, x_min, x_max, y_min, y_max, povrsina):
     rezultati = []
     for video_path in video_folder.iterdir():
 
         if video_path.suffix.lower() not in VIDEO_EXTENSIONS:
             continue
-        row = analiza_jednog_videa(video_path, maska)
+        row = analiza_jednog_videa(video_path, maska, x_min, x_max, y_min, y_max, povrsina)
         if row is not None:
             rezultati.append(row)
 
@@ -142,14 +164,40 @@ def analiza_svih_videa(video_folder, maska):
 
 def main():
     start_time = timer()
+
+    start_time1 = timer()
     video = VIDEO_PATH / "bez_pokreta1.mp4"
-    maska = roi_maska(video)
-    rezultati = analiza_svih_videa(VIDEO_PATH, maska)
+    maska, x_min, x_max, y_min, y_max, povrsina = roi_maska(video)
+    end_time1 = timer()
+
+    """
+    filename = video.name
+    cap = cv2.VideoCapture(str(video))
+
+    ret, frame = cap.read()
+
+    sivi_frejm = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    bbox_frejm = sivi_frejm[y_min:y_max, x_min:x_max] * maska[y_min:y_max, x_min:x_max]
+    print(povrsina)
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(maska)
+    plt.subplot(1,2,2)
+    plt.imshow(bbox_frejm)
+    plt.show()
+    """
+
+    start_time2 = timer()
+    rezultati = analiza_svih_videa(VIDEO_PATH, maska, x_min, x_max, y_min, y_max, povrsina)
     df = pd.DataFrame(rezultati)
     OUT_PATH.parent.mkdir(parents=True, exist_ok=True)
     df.to_csv(OUT_PATH, index=False)
+    end_time2 = timer()
+
     end_time = timer()
-    print(f"Gascina sve radi kako treba, vreme potrebno za izvrsavanje {end_time-start_time}")
+    print(f"Gascina sve radi kako treba, ukupno vreme potrebno za izvrsavanje {end_time-start_time} \n"
+          f"vreme potrebno za biranje ROI {end_time1-start_time1}\n"
+          f"vreme potrebno za analiza svih videa {end_time2-start_time2}\n")
 
 
 if __name__ == "__main__":
--- app/mkdirs.py
+++ app/mkdirs.py
@@ -9,4 +9,4 @@ RAW_VIDEOS = DATA / "raw_videos"
 direktorijumi = [DATA, PROCESSED, RAW_VIDEOS]
 
 for direktorijum in direktorijumi:
-    direktorijum.mkdir(parents=True, exist_ok=False)
+    direktorijum.mkdir(parents=True, exist_ok=True)
